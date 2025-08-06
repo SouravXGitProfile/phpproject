7081db978066c4d5cb65b66f352a478c9cfd05a9
sourav santra <s.souravsantra2023@gmail.com>
2025-08-07 00:44:23 +0530

    First commit - added my PHP project

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial part</title>
+</head>
+<body>
+    <div class="container">
+        hello its my first websit
+        <?php
+       
+       echo "hello world and this printed using php";
+
+        $variable=34;
+        $variable1=67;
+        echo $variable;
+        echo $variable1;
+        //variable 
+        
+
+
+
+        ?>
+    </div>
+    
+</body>
+</html>
